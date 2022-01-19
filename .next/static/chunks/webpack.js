10a59789fba1f9761a0b93210baa449f8810bc74
Vasily93 <markovavasily@gmail.com>
2022-01-19 14:19:25 -0800

    refactored to have project card as a component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7fdd44d22b585f93"; }
+/******/ 		__webpack_require__.h = function() { return "c9fb9fb07ba33fb8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
