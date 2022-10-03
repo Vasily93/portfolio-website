77251d7eb6300fd3b4a8af07d5b0c76e26cde7ea
Vasily93 <markovavasily@gmail.com>
2022-10-02 17:46:02 -0700

    added gallery project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0bf2824a97ac98e21672"; }
+/******/ 		__webpack_require__.h = function() { return "ee19bb800ef951d6cc4e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
