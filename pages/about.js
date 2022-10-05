672490fdc7ed5211d120b321de9f1b2781059f9f
Vasily93 <markovavasily@gmail.com>
2022-10-05 14:58:43 -0700

    mvp for about page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import Button from '@mui/material/Button';
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box, Typography, Avatar, Fab } from '@mui/material';
+import { Stack } from '@mui/system';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 
 const AboutMe = () => {
     
@@ -9,24 +13,40 @@ const AboutMe = () => {
       <Box
       sx={{
         bgcolor: 'background.paper',
-        pt: 8,
-        pb: 46,
+        pt: 2,
+        pb: 4,
+        pr: 2,
         margin: '20px'
       }}
     >
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="text.primary"
-          gutterBottom
-        >
-          Vasily Markov
-        </Typography>
-        <Typography variant="h5" align="center" color="text.secondary" paragraph>
-          I am Front End Javascript Web Developer. React Oriented. Open for oppotunities!
-          Check out my latest projects below.
-        </Typography>
+      <Container maxWidth='sm' sx={{ width: '150px', pb: 2 }}>
+        <Avatar alt='avatar image' src='/avatar.jpeg' sx={{ width: 100, height: 100 , boxShadow: 5 }}/>
+      </Container>
+      <Typography
+        component="h1"
+        variant="h4"
+        align="center"
+        color="text.primary"
+        gutterBottom
+      >
+        Vasily Markov
+      </Typography>
+      <Typography variant="h5" align="center" color="text.secondary" paragraph>
+        I am Front-end Javascript Web Developer, React Oriented. I am pacinate to 
+        create solutions for businesses and automate processes. 
+        Open for oppotunities!
+      </Typography>
+      <Container sx={{ width: '185px' }}>
+        <Stack direction='row' maxWidth='sm' spacing={2}>
+          <Fab href='https://github.com/Vasily93' color='primary' size='large'>
+            <GitHubIcon fontSize='large' />
+          </Fab>
+          <Fab href='https://www.linkedin.com/in/vasily-markov/' color='primary' size='large'>
+            <LinkedInIcon fontSize='large' />
+          </Fab>
+        </Stack>
+      </Container>
+
     </Box>
     </Container>
   )
