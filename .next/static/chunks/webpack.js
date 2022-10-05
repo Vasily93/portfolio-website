8aad684abdb49401c370a68ce4f85118b0375d90
Vasily93 <markovavasily@gmail.com>
2022-10-05 15:18:23 -0700

    added name and favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "379d25f1b4cafa04"; }
+/******/ 		__webpack_require__.h = function() { return "6f1c19e9b59fbbfe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <link rel="icon" type="image" href="/faviton.png" />
+      <title>Vasily Markov</title>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
